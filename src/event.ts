2319f5e8c4e38c8dc1757d5be64671abb3406f26
David Morcillo <david.morcillo@factorial.co>
2022-08-29 14:50:18 +0200

    Add more workflow events

--- src/event.ts
+++ src/event.ts
@@ -1,9 +1,17 @@
-export type EventName = "push" | "pull_request";
+export type EventName =
+  | "push"
+  | "pull_request"
+  | "pull_request_review"
+  | "workflow_run";
 
 export type EventOptions<T extends EventName> = T extends "push"
   ? PushEventOptions
   : T extends "pull_request"
   ? PullRequestEventOptions
+  : T extends "pull_request_review"
+  ? PullRequestReviewEventOptions
+  : T extends "workflow_run"
+  ? WorkflowRunEventOptions
   : never;
 
 interface PushEventOptions {
@@ -12,8 +20,18 @@ interface PushEventOptions {
 }
 
 interface PullRequestEventOptions {
-  types?: Array<"opened" | "reopened" | "synchronize">;
+  types?: Array<
+    "opened" | "reopened" | "synchronize" | "labeled" | "unlabeled"
+  >;
   paths?: string[];
+}
+
+interface PullRequestReviewEventOptions {
+  types?: Array<"submitted">;
+}
+interface WorkflowRunEventOptions {
+  workflows?: string[];
+  types?: Array<"completed">;
 }
 
 export interface Event {
