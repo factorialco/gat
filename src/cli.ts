698c7067c3dea1c21f20150895a7cb58dd867f58
David Morcillo <david.morcillo@factorial.co>
2022-10-04 13:06:53 +0200

    Add watch command
    This adds a new command to the CLI to watch file changes and compile templates automatically.

--- src/cli.ts
+++ src/cli.ts
@@ -4,11 +4,14 @@ import path from "path";
 import { exec } from "child_process";
 import { Command } from "commander";
 import { promisify } from "util";
+import debounce from "lodash/debounce"
 
 const execPromise = promisify(exec);
 const writeFilePromise = promisify(fs.writeFile);
+const folder = path.join(process.cwd(), ".github", "templates");
 
 const parseFile = async (templateFile: string) => {
+  // NOTE: can we improve this using ts-node or typescript programatically?
   const { stdout } = await execPromise(`npx ts-node ${templateFile}`);
 
   await writeFilePromise(
@@ -24,15 +27,13 @@ const parseFile = async (templateFile: string) => {
 
 const cli = new Command();
 
-cli.version("0.0.1").description("TODO");
+cli.version("1.0.0").description("Write your GitHub Actions workflows using TypeScript");
 
 cli
   .command("build")
-  .description("TODO")
-  .argument("[file]", "TODO")
+  .description("Transpile all Gat templates into GitHub Actions workflows.")
+  .argument("[file]", "(Optional) A Gat template file")
   .action(async (file) => {
-    const folder = path.join(process.cwd(), ".github", "templates");
-
     if (!fs.existsSync(path.join(folder, "..", "workflows"))) {
       fs.mkdirSync(path.join(folder, "..", "workflows"));
     }
@@ -51,5 +52,21 @@ cli
 
     process.exit(0);
   });
+
+cli
+  .command("watch")
+  .description("Watch file changes in your Gat templates folder and transpile them automatically.")
+  .action(async () => {
+    const parseWatchedFile = debounce(async (fileName) => {
+      const start = process.hrtime.bigint()
+      process.stdout.write(`😸 Detected change on file ${fileName}. Transpiling... `)
+      await parseFile(path.join(folder, fileName.toString()));
+      console.log(`Done in ${((Number(process.hrtime.bigint() - start)) / 1_000_000).toFixed(2)}ms`)
+    }, 1000)
+    console.log(`😼 Watching file changes on ${folder}...`)
+    fs.watch(folder).on("change", (_eventName, fileName) => {
+      parseWatchedFile(fileName)
+    })
+  })
 
 cli.parse(process.argv);
